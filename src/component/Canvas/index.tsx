f1e384fde715cb7a8a02b47ccca180a072c53df3
Aleksandr Miroshnnichenko <miroshnichenko.megagroup@gmail.com>
2024-06-13 14:29:23 +0300

    Add Canvas

--- src/component/Canvas/index.tsx
+++ src/component/Canvas/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from 'react';
+import { FC, useLayoutEffect, useRef } from 'react';
 import { IFields } from '../FontParams';
 import './index.css';
 
@@ -6,12 +6,30 @@ export interface ICanvasProps extends IFields {
   fonts: string[];
 }
 
-export const Canvas: FC<ICanvasProps> = () => {
-  const canvasRef = useRef(null);
+export const Canvas: FC<ICanvasProps> = ({ color, fonts, fontSize, padding }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const indent = 10;
+  const width = 300;
+  const height = fonts.length * (fontSize + padding) + padding;
+
+  useLayoutEffect(() => {
+    const context = canvasRef.current?.getContext('2d');
+
+    if (context) {
+      context.clearRect(0, 0, width, height);
+      context.textBaseline = 'top';
+      context.fillStyle = color;
+
+      fonts.forEach((text, index) => {
+        context.font = `${fontSize}px "${text}"`;
+        context.fillText(text, indent, index * (fontSize + padding) + padding, width - 2 * indent);
+      })
+    }
+  }, [color, fonts, fontSize, padding, height]);
 
   console.log(canvasRef);
 
   return (
-    <canvas id="canvas" ref={canvasRef.current} width={400} height={1000} />
+    <canvas ref={canvasRef} width={width} height={height} />
   );
 }
